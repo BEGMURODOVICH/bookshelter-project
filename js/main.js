c65d9c1274ea0da240b7f234e3f6084d8465e049
Azizbek Begmurodovich Sayfiyev <azizbekofficialaccaunt@gmail.com>
2024-12-13 17:38:17 +0500

    json.api

--- js/main.js
+++ js/main.js
@@ -1,23 +1,73 @@
 const modalClose = document.getElementById("modalClose");
 const moreModal = document.querySelector(".more-modal");
 const modalContent = document.querySelector(".modal-content");
-const moreInfoBtn = document.querySelectorAll(".moreInfoBtn");
+const bookUl = document.querySelector(".books");
 
-moreInfoBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    moreModal.style.display = "block";
-    moreModal.style.background = "#00000080";
-    moreModal.style.transition = "all 1s ease";
-    modalContent.style.transition = "ease all 1s ";
-    modalContent.style.transition = "ease all 1s";
-    modalContent.style.transform = "translateX(0)";
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((posts) => {
+    const resultP = document.querySelector(".resultP");
+    const postsCount = posts.length;
+    resultP.textContent = `Showing ${postsCount} Result(s)`;
+
+    posts.forEach((post) => {
+      const bookElement = document.createElement("li");
+      bookElement.innerHTML = `<div class="book-info">
+          <img src="img/book1.svg" alt="" />
+          <div class="info">
+            <h3>${post.title.slice(0, 5)}</h3>
+            <p>${post.body.slice(0, 10)}</p>
+            <p>${post.id}</p>
+          </div>
+          <div class="book-btn">
+            <div class="book-action">
+              <button class="bookmarkBtn">Bookmark</button>
+              <button class="moreInfoBtn">More Info</button>
+            </div>
+            <button class="readBtn">Read</button>
+          </div>
+        </div>`;
+
+      bookUl.appendChild(bookElement);
+    });
+
+    bookUl.addEventListener("click", (event) => {
+      if (event.target.classList.contains("moreInfoBtn")) {
+        moreModal.style.display = "block";
+        moreModal.style.background = "#00000080";
+        moreModal.style.transition = "all 1s ease";
+        modalContent.style.transition = "ease all 1s";
+        modalContent.style.transform = "translateX(0)";
+      }
+    });
   });
-});
 
 modalClose.addEventListener("click", () => {
   moreModal.style.display = "none";
 });
+
 const logOut = document.querySelector(".logOut");
 logOut.addEventListener("click", () => {
   window.location.href = "login-sigin.html";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+
+  if (!username || !password) {
+    window.location.href = "login-sigin.html";
+  }
+});
+
+bookUl.addEventListener("click", (event) => {
+  if (event.target.closest(".book-item")) {
+    const postElement = event.target.closest(".book-item");
+    const title = postElement.querySelector(".info h3").textContent;
+    const body = postElement.querySelector(".info p").textContent;
+    const id = postElement.querySelector(".info + p").textContent;
+    console.log(`Post ID: ${id}`);
+    console.log(`Title: ${title}`);
+    console.log(`Body: ${body}`);
+  }
 });
--- js/registration.js
+++ js/registration.js
@@ -12,3 +12,30 @@ gologin.addEventListener("click", () => {
   signin.style.display = "none";
   login.style.display = "block";
 });
+
+// sigin
+document.getElementById("sigin-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("signin-username").value;
+  const password = document.getElementById("signin-password").value;
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
+});
+
+// login
+document.getElementById("login-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputUsername = document.getElementById("login-username").value;
+  const inputPassword = document.getElementById("login-password").value;
+
+  const storedUsername = localStorage.getItem("username");
+  const storedPassword = localStorage.getItem("password");
+
+  if (inputUsername === storedUsername && inputPassword === storedPassword) {
+    window.location.href = "index.html";
+  } else {
+    console.log("xato");
+  }
+});
