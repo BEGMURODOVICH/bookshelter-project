f34b40bd80af702d354dd6cb9e3fcbf367ce9e66
Azizbek Begmurodovich Sayfiyev <azizbekofficialaccaunt@gmail.com>
2024-12-13 16:31:10 +0500

    modal

--- js/main.js
+++ js/main.js
@@ -5,13 +5,18 @@ const moreInfoBtn = document.querySelectorAll(".moreInfoBtn");
 
 moreInfoBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    console.log("salom");
-    modalContent.classList.add("modal-content-active");
+    moreModal.style.display = "block";
     moreModal.style.background = "#00000080";
     moreModal.style.transition = "all 1s ease";
+    modalContent.style.transition = "ease all 1s ";
+    modalContent.style.transition = "ease all 1s";
+    modalContent.style.transform = "translateX(0)";
   });
 });
 
+modalClose.addEventListener("click", () => {
+  moreModal.style.display = "none";
+});
 const logOut = document.querySelector(".logOut");
 logOut.addEventListener("click", () => {
   window.location.href = "login-sigin.html";
